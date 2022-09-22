27ffd55978ed81eeda20f8310b191815490e29f7
BAGUVIX-1 <bagoyuz2003@mail.ru>
2022-09-22 16:20:25 +0300

    Добавление Debug

--- ModulPm/Program.cs
+++ ModulPm/Program.cs
@@ -15,12 +15,8 @@ namespace calcul
             while (true)
             {
                 Console.WriteLine("Какое действие выберите? \n1. Сумма\n2. Вычитание\n3. Умножение\n4. Деление");
-
-
                 try //Добавление конструкции TryCatch для защиты от ввода букв
                 {
-                    StreamWriter sw = new StreamWriter("debuginfo.txt");
-                    sw.WriteLine("У пользователя появилось приветственное сообщение");
                     char choice = '0'; //Объявление переменной Choice
                     double a = 0; //Объявление переменной a
                     double b = 0; //Объявление переменной b
@@ -28,19 +24,14 @@ namespace calcul
                     Debug.WriteLine("Пользователь получил сообщение о выборе действия");
                     choice = Convert.ToChar(Console.ReadLine());
                     Debug.WriteLine("Пользователь выбрал действие");
-                    sw.WriteLine("Пользователь выбрал действие");
                     Console.WriteLine("Введите первое число:");
                     a = Convert.ToDouble(Console.ReadLine()); //Пользователь вводит первое число
                     Debug.WriteLine("Пользователь ввел первое число");
-                    sw.WriteLine("Пользователь ввел первое число");
                     Console.WriteLine("Введите второе число:");
                     b = Convert.ToDouble(Console.ReadLine()); //Пользователь вводит второе число
                     Debug.WriteLine("Пользователь ввел второе число");
-                    sw.WriteLine("Пользователь ввел второе число");
-
                     result = 0;
                     Methods methods = new Methods();
-
                     if (choice == '1')
                     {
                         result = methods.Amount(a, b);
@@ -61,18 +52,13 @@ namespace calcul
                     {
                         Console.WriteLine("Пожалуйста, введите значение от 1 до 4");
                     }
-
                     Console.WriteLine($"Результат: {result}");
                     Debug.WriteLine("Пользователь получил результат");
-                    sw.WriteLine("Пользователь получил результат");
-                    sw.Close();
                 }
                 catch
                 {
                     Console.WriteLine("Пожалуйста, вводите только цифры, нажмите кнопку для продолжения работы");
                 }
-
-
                 Console.ReadKey();
             }
         }
