80881cc0ea46ebd903cdf34b5e42ba63df0ecd48
BAGUVIX-1 <bagoyuz2003@mail.ru>
2022-09-22 16:15:01 +0300

    Создан отдельный класс Methods для методов

---
+++ ModulPm/Methods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace calcul
+{
+    /// <summary>
+    /// Здесь объявлены все методы
+    /// </summary>
+    public class Methods
+    {
+        public double Amount(double a, double b) { return a + b; }
+        public double Subtraction(double a, double b) { return a - b; }
+        public double Multiplication(double a, double b) { return a * b; }
+        public double Divide(double a, double b) { return a / b; }
+    }
+}
--- ModulPm/Program.cs
+++ ModulPm/Program.cs
@@ -1,16 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
-namespace ModulPm
+namespace calcul
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Выполнение основных функций калькулятора 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Какое действие выберите? \n1. Сумма\n2. Вычитание\n3. Умножение\n4. Деление");
 
+
+                try //Добавление конструкции TryCatch для защиты от ввода букв
+                {
+                    StreamWriter sw = new StreamWriter("debuginfo.txt");
+                    sw.WriteLine("У пользователя появилось приветственное сообщение");
+                    char choice = '0'; //Объявление переменной Choice
+                    double a = 0; //Объявление переменной a
+                    double b = 0; //Объявление переменной b
+                    double result = 0;
+                    Debug.WriteLine("Пользователь получил сообщение о выборе действия");
+                    choice = Convert.ToChar(Console.ReadLine());
+                    Debug.WriteLine("Пользователь выбрал действие");
+                    sw.WriteLine("Пользователь выбрал действие");
+                    Console.WriteLine("Введите первое число:");
+                    a = Convert.ToDouble(Console.ReadLine()); //Пользователь вводит первое число
+                    Debug.WriteLine("Пользователь ввел первое число");
+                    sw.WriteLine("Пользователь ввел первое число");
+                    Console.WriteLine("Введите второе число:");
+                    b = Convert.ToDouble(Console.ReadLine()); //Пользователь вводит второе число
+                    Debug.WriteLine("Пользователь ввел второе число");
+                    sw.WriteLine("Пользователь ввел второе число");
+
+                    result = 0;
+                    Methods methods = new Methods();
+
+                    if (choice == '1')
+                    {
+                        result = methods.Amount(a, b);
+                    }
+                    else if (choice == '2')
+                    {
+                        result = methods.Subtraction(a, b);
+                    }
+                    else if (choice == '3')
+                    {
+                        result = methods.Multiplication(a, b);
+                    }
+                    else if (choice == '4')
+                    {
+                        result = methods.Divide(a, b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пожалуйста, введите значение от 1 до 4");
+                    }
+
+                    Console.WriteLine($"Результат: {result}");
+                    Debug.WriteLine("Пользователь получил результат");
+                    sw.WriteLine("Пользователь получил результат");
+                    sw.Close();
+                }
+                catch
+                {
+                    Console.WriteLine("Пожалуйста, вводите только цифры, нажмите кнопку для продолжения работы");
+                }
+
+
+                Console.ReadKey();
+            }
         }
     }
 }
